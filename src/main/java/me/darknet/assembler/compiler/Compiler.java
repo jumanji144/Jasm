a99d2edf7b1ec924f70d4b174d494ccf6f694f09
Justus Garbe <gihihoh@gmail.com>
2022-05-05 18:51:43 +0200

    Refactor of method compiling system

--- src/main/java/me/darknet/assembler/compiler/Compiler.java
+++ src/main/java/me/darknet/assembler/compiler/Compiler.java
@@ -21,9 +21,7 @@ public class Compiler {
 
     public String className;
 
-    public Map<String, Label> labels = new HashMap<>();
-    public List<String> locals = new ArrayList<>();
-    public boolean inStatic = false;
+    public List<Method> methods = new ArrayList<>();
 
     public Compiler(int version) {
         cw = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
@@ -61,144 +59,28 @@ public class Compiler {
                 }
                 case METHOD_DECLARATION: {
                     int access = getAccess(g.getChild(GroupType.ACCESS_MODS));
-                    if((access | ACC_STATIC) == access) {
-                        inStatic = true;
-                    }
                     String methodDesc = g.get(1).content();
 
                     MethodDescriptor md = new MethodDescriptor(methodDesc);
                     MethodVisitor vs = cw.visitMethod(access, md.name, md.desc, null, null);
 
+                    boolean isStatic = (access & ACC_STATIC) != 0;
+
+                    Method method = new Method(vs, md.name, isStatic);
+
                     Group body = g.getChild(GroupType.BODY);
-                    if (body != null) {
-                        for (Group inst : body.getChildren()) {
-                            if (inst.type == GroupType.LABEL) {
-                                Label l = getLabel(inst.content().replace(":", ""));
-                                vs.visitLabel(l);
-                            } else if (inst.type == GroupType.LOOKUP_SWITCH) {
-                                Group defaultGroup = inst.getChild(GroupType.DEFAULT_LABEL);
-                                if(defaultGroup == null) {
-                                    throw new AssemblerException("Lookup switch must have a default label", inst.start().getLocation());
-                                }
-                                Label defaultLabel = getLabel(defaultGroup.get(0).content());
-                                int[] keys = new int[inst.size() - 1];
-                                Label[] labels = new Label[inst.size() - 1];
-                                for(int i = 0; i < inst.size() - 1; i++) {
-                                    Group caseGroup = inst.get(i);
-                                    if(caseGroup.type == GroupType.CASE_LABEL) {
-                                        Label caseLabel = getLabel(caseGroup.get(1).content());
-                                        int key = Integer.parseInt(caseGroup.get(0).content());
-                                        labels[i] = caseLabel;
-                                        keys[i] = key;
-                                    }
-                                }
-                                vs.visitLookupSwitchInsn(defaultLabel, keys, labels);
-                            } else if(inst.type == GroupType.TABLE_SWITCH) {
-                                Group defaultGroup = inst.getChild(GroupType.DEFAULT_LABEL);
-                                if(defaultGroup == null) {
-                                    throw new AssemblerException("Table switch must have a default label", inst.start().getLocation());
-                                }
-                                Label defaultLabel = getLabel(defaultGroup.get(0).content());
-                                List<Label> labels = new ArrayList<>();
-                                for(Group caseGroup : inst.children) {
-                                    if(caseGroup.type == GroupType.IDENTIFIER) {
-                                        Label caseLabel = getLabel(caseGroup.content());
-                                        labels.add(caseLabel);
-                                    }
-                                }
-                                vs.visitTableSwitchInsn(0, labels.size() - 1, defaultLabel, labels.toArray(new Label[0]));
-                            } else if (inst.type == GroupType.INSTRUCTION) {
-                                visitInstruction(vs, inst);
-                            } else {
-                                throw new AssemblerException("Unknown instruction type: " + inst.type, inst.value.getLocation());
-                            }
-                        }
-                    }
+                    method.compile(body);
 
+                    method.verify();
 
+                    methods.add(method);
 
                     vs.visitMaxs(0, 0);
                     vs.visitEnd();
-
-                    locals.clear();
                 }
             }
         }
 
-    }
-
-    public void visitInstruction(MethodVisitor mv, Group g) throws AssemblerException {
-        ParseInfo info = ParseInfo.get(g.content());
-        if (info == null) throw new RuntimeException("Unknown instruction: " + g.content());
-        if(info.name.contains("_")) {
-            if(info.name.contains("store") || info.name.contains("load")) {
-                int opcode = info.opcode;
-                int index = Integer.parseInt(info.name.substring(info.name.indexOf("_") + 1));
-                mv.visitVarInsn(opcode, index);
-                return;
-            }
-        }
-        int opcode = info.opcode;
-        switch (opcode) {
-            case INVOKEVIRTUAL:
-            case INVOKESTATIC:
-            case INVOKESPECIAL: {
-                MethodDescriptor md = new MethodDescriptor(g.get(0).content());
-                String owner = md.owner == null ? className : md.owner;
-                mv.visitMethodInsn(opcode, owner, md.name, md.desc, false);
-                break;
-            }
-            case GETFIELD:
-            case GETSTATIC:
-            case PUTFIELD:
-            case PUTSTATIC: {
-                FieldDescriptor fd = new FieldDescriptor(g.get(0).content());
-                String owner = fd.owner == null ? className : fd.owner;
-                mv.visitFieldInsn(opcode, owner, fd.name, g.get(1).content());
-                break;
-            }
-            case IF_ICMPEQ:
-            case IF_ICMPNE:
-            case IF_ICMPLT:
-            case IF_ICMPGE:
-            case IF_ICMPGT:
-            case IF_ICMPLE:
-            case IF_ACMPEQ:
-            case IF_ACMPNE:
-            case GOTO: {
-                mv.visitJumpInsn(opcode, getLabel(g.get(0).content()));
-                break;
-            }
-            case LDC: {
-                mv.visitLdcInsn(g.get(0).content());
-                break;
-            }
-            case ALOAD:
-            case ILOAD:
-            case FLOAD:
-            case DLOAD:
-            case LLOAD:
-            case ASTORE:
-            case ISTORE:
-            case FSTORE:
-            case DSTORE:
-            case LSTORE: {
-                mv.visitVarInsn(opcode, getLocal(g.get(0), true));
-                break;
-            }
-            case IINC: {
-                mv.visitIincInsn(getLocal(g.get(0), false), Integer.parseInt(g.get(1).content()));
-                break;
-            }
-            case SIPUSH:
-            case BIPUSH: {
-                mv.visitIntInsn(opcode, Integer.parseInt(g.get(0).content()));
-                break;
-            }
-            default: {
-                mv.visitInsn(opcode);
-            }
-        }
     }
 
     public int getAccess(Group access) {
@@ -219,49 +101,7 @@ public class Compiler {
         return accessFlags;
     }
 
-    public Label getLabel(String label) {
-        Label l = labels.get(label);
-        if (l == null) {
-            l = new Label();
-            labels.put(label, l);
-        }
-        return l;
-    }
-
-    public int getLocal(Group g, boolean create) throws AssemblerException {
-        if (g.type != GroupType.IDENTIFIER) {
-            if (g.type == GroupType.NUMBER) {
-                return Integer.parseInt(g.content());
-            } else
-                throw new AssemblerException("Expected identifier", g.start().getLocation());
-        }
-        String name = g.content();
-        if (Objects.equals(name, "this")) return 0;
-        int index = locals.indexOf(name);
-        if (index == -1) {
-            if (create) {
-                locals.add(name);
-                index = locals.size() - 1;
-            } else {
-                throw new AssemblerException("Unknown local variable: " + name, g.start().getLocation());
-            }
-        }
-        if(inStatic) return index;
-        return index + 1;
-    }
-
-    public void verify() throws AssemblerException {
-        for (var stringLabelEntry : labels.entrySet()) {
-            try {
-                stringLabelEntry.getValue().getOffset();
-            } catch (IllegalStateException e) {
-                throw new AssemblerException("Unknown label: " + stringLabelEntry.getKey(), null);
-            }
-        }
-    }
-
     public byte[] finish() throws AssemblerException {
-        verify();
         return cw.toByteArray();
     }
 
