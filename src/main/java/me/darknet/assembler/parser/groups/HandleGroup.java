831780c3d3aa44b76637fbade5975a20580f0977
Justus Garbe <gihihoh@gmail.com>
2022-05-17 00:06:45 +0200

    Support for field handles

--- src/main/java/me/darknet/assembler/parser/groups/HandleGroup.java
+++ src/main/java/me/darknet/assembler/parser/groups/HandleGroup.java
@@ -10,9 +10,22 @@ public class HandleGroup extends Group {
     IdentifierGroup handleType;
     IdentifierGroup descriptor;
 
+    IdentifierGroup fieldDescriptor;
+
     public HandleGroup(Token value, IdentifierGroup type, IdentifierGroup descriptor) {
         super(GroupType.HANDLE, value, type, descriptor);
         this.handleType = type;
         this.descriptor = descriptor;
+    }
+
+    public HandleGroup(Token value, IdentifierGroup type, IdentifierGroup descriptor, IdentifierGroup fieldDescriptor) {
+        super(GroupType.HANDLE, value, type, descriptor);
+        this.handleType = type;
+        this.descriptor = descriptor;
+        this.fieldDescriptor = fieldDescriptor;
+    }
+
+    public boolean isField() {
+        return fieldDescriptor != null;
     }
 }
