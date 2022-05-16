1cb669c05fdfad235419ee47a78ce696de20e191
Justus Garbe <gihihoh@gmail.com>
2022-05-17 00:08:21 +0200

    Support for hex parsing

--- src/main/java/me/darknet/assembler/parser/groups/NumberGroup.java
+++ src/main/java/me/darknet/assembler/parser/groups/NumberGroup.java
@@ -11,23 +11,28 @@ public class NumberGroup extends Group {
 
     public Number getNumber() {
         String value = content();
+        int radix = 10;
+        if(value.startsWith("0x") || value.startsWith("0X")){
+            radix = 16;
+            value = value.substring(2);
+        }
         if (value.contains(".")) {
-            if(value.endsWith("f")) {
+            if(value.endsWith("f") || value.endsWith("F")) {
                 return Float.parseFloat(value.substring(0, value.length() - 1));
             }
             return Double.parseDouble(value);
         }else {
             if(value.endsWith("L")) {
-                return Long.parseLong(value.substring(0, value.length() - 1));
+                return Long.parseLong(value.substring(0, value.length() - 1), radix);
             }
-            return Integer.parseInt(value);
+            return Integer.parseInt(value, radix);
         }
     }
 
     public boolean isWide() {
         String value = content();
         if (value.contains(".")) {
-            return !value.endsWith("f");
+            return !value.endsWith("f") && !value.endsWith("F");
         } else {
             return value.endsWith("L");
         }
