391e81cbebdf449720f55a7cf254cf1c0113b969
Col-E <mcoley2@gmu.edu>
2022-11-12 14:19:35 -0500

    Update sample code in test cases

--- src/test/java/me/darknet/assembler/CompilerTests.java
+++ src/test/java/me/darknet/assembler/CompilerTests.java
@@ -1,7 +1,6 @@
 package me.darknet.assembler;
 
 import me.darknet.assembler.parser.AssemblerException;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -12,25 +11,24 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Disabled("The test cases are out of date, the format has since changed")
 public class CompilerTests {
-    @ParameterizedTest
-    @MethodSource("paths")
-    public void testExample(Path path) throws Exception {
-        String name = path.getFileName().toString();
-        byte[] bytes = Files.readAllBytes(path);
-        String source = new String(bytes);
-        System.out.print("Testing: " + name);
-        try {
-            Assembler.assemble(source, 52, bytes);
-        } catch (AssemblerException e) {
-            throw new RuntimeException(e.describe(), e);
-        }
-    }
+	@ParameterizedTest
+	@MethodSource("paths")
+	public void testExample(Path path) throws Exception {
+		String name = path.getFileName().toString();
+		byte[] bytes = Files.readAllBytes(path);
+		String source = new String(bytes);
+		System.out.print("Testing: " + name);
+		try {
+			Assembler.assemble(source, 52, bytes);
+		} catch (AssemblerException e) {
+			throw new RuntimeException(e.describe(), e);
+		}
+	}
 
-    public static List<Path> paths() throws IOException {
-        return Files.walk(Paths.get("src/test/resources/"))
-                .filter(p -> p.toString().endsWith(".ja"))
-                .collect(Collectors.toList());
-    }
+	public static List<Path> paths() throws IOException {
+		return Files.walk(Paths.get("src/test/resources/"))
+				.filter(p -> p.toString().endsWith(".ja"))
+				.collect(Collectors.toList());
+	}
 }
