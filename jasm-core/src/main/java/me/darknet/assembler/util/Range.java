cadb6c3edd4d7c673f3708aefa96f848271b6833
Justus Garbe <gihihoh@gmail.com>
2023-07-19 19:48:18 +0200

    refactor(utils): made range record

--- jasm-core/src/main/java/me/darknet/assembler/util/Range.java
+++ jasm-core/src/main/java/me/darknet/assembler/util/Range.java
@@ -1,21 +1,3 @@
 package me.darknet.assembler.util;
 
-public class Range {
-
-	private int start;
-	private int end;
-
-	public Range(int start, int end) {
-		this.start = start;
-		this.end = end;
-	}
-
-	public int getStart() {
-		return start;
-	}
-
-	public int getEnd() {
-		return end;
-	}
-
-}
+public record Range(int start, int end) { }
