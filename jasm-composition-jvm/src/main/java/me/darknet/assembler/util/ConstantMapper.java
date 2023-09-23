6dbd747b7cd1cf6c65354588830cb4c08625d71e
Col-E <mcoley2@gmu.edu>
2023-09-23 05:56:45 -0400

    Fix not considering method-handles to primitive fields in constant mapper

--- jasm-composition-jvm/src/main/java/me/darknet/assembler/util/ConstantMapper.java
+++ jasm-composition-jvm/src/main/java/me/darknet/assembler/util/ConstantMapper.java
@@ -1,10 +1,7 @@
 package me.darknet.assembler.util;
 
 import dev.xdark.blw.constant.*;
-import dev.xdark.blw.type.MethodHandle;
-import dev.xdark.blw.type.ObjectType;
-import dev.xdark.blw.type.Type;
-import dev.xdark.blw.type.Types;
+import dev.xdark.blw.type.*;
 import me.darknet.assembler.ast.ASTElement;
 import me.darknet.assembler.ast.primitive.ASTArray;
 import me.darknet.assembler.ast.primitive.ASTIdentifier;
@@ -24,7 +21,13 @@ public class ConstantMapper {
 
         ObjectType owner = Types.instanceTypeFromInternalName(className);
 
-        Type methodType = kind.isField() ? Types.instanceTypeFromDescriptor(descriptor) : Types.methodType(descriptor);
+        // Despite the naming conventions, you can have a method handle to a field.
+        // Field types can be arrays, classes, or primitives.
+        // To keep things simple in this case we'll use type-reader directly rather than chaining
+        //  conditional type util calls.
+        Type methodType = kind.isField() ?
+                new TypeReader(descriptor).read() :
+                Types.methodType(descriptor);
 
         // TODO: ITF
         return new MethodHandle(kind.ordinal(), owner, methodName, methodType, false);
