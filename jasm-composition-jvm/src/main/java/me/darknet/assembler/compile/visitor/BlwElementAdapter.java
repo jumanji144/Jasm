3ef58dd04060b5bbe222a9ee9ce0522cba864e49
Justus Garbe <gihihoh@gmail.com>
2023-11-04 13:53:06 +0100

    fix: correct parsing of number elements

--- jasm-composition-jvm/src/main/java/me/darknet/assembler/compile/visitor/BlwElementAdapter.java
+++ jasm-composition-jvm/src/main/java/me/darknet/assembler/compile/visitor/BlwElementAdapter.java
@@ -1,9 +1,8 @@
 package me.darknet.assembler.compile.visitor;
 
-import dev.xdark.blw.annotation.Element;
-import dev.xdark.blw.annotation.ElementDouble;
-import dev.xdark.blw.annotation.ElementString;
+import dev.xdark.blw.annotation.*;
 import me.darknet.assembler.ast.ElementType;
+import me.darknet.assembler.ast.primitive.ASTNumber;
 import me.darknet.assembler.ast.specific.ASTValue;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,7 +12,23 @@ public interface BlwElementAdapter {
 		ElementType valueType = value.type();
 		return switch (valueType) {
 			case STRING -> new ElementString(value.content());
-			case NUMBER -> new ElementDouble(Double.parseDouble(value.content()));
+			case NUMBER -> {
+				ASTNumber number = (ASTNumber) value;
+				if(number.isFloatingPoint()) {
+					if(number.isWide()) {
+						yield new ElementDouble(number.asDouble());
+					} else {
+						yield new ElementFloat(number.asFloat());
+					}
+				} else {
+					if(number.isWide()) {
+						yield new ElementLong(number.asLong());
+					} else {
+						yield new ElementInt(number.asInt());
+					}
+				}
+			}
+			case BOOL -> new ElementBoolean(Boolean.parseBoolean(value.content()));
 			default -> throw new UnsupportedOperationException("Enum value of type not supported yet: " + valueType);
 		};
 	}
