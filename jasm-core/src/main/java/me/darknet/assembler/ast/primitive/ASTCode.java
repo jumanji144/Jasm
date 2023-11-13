0c2fd431475c24cbb04cb738485da287d3d00037
Col-E <mcoley2@gmu.edu>
2023-11-13 08:40:05 -0500

    ASTCode's instruction should not contain null values

--- jasm-core/src/main/java/me/darknet/assembler/ast/primitive/ASTCode.java
+++ jasm-core/src/main/java/me/darknet/assembler/ast/primitive/ASTCode.java
@@ -2,20 +2,21 @@ package me.darknet.assembler.ast.primitive;
 
 import me.darknet.assembler.ast.ASTElement;
 import me.darknet.assembler.ast.ElementType;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
 public class ASTCode extends ASTElement {
 
-    private final List<@Nullable ASTInstruction> instructions;
+    private final List<@NotNull ASTInstruction> instructions;
 
-    public ASTCode(List<@Nullable ASTInstruction> instructions) {
+    public ASTCode(List<@NotNull ASTInstruction> instructions) {
         super(ElementType.CODE, instructions);
         this.instructions = instructions;
     }
 
-    public List<@Nullable ASTInstruction> instructions() {
+    public List<@NotNull ASTInstruction> instructions() {
         return instructions;
     }
 
