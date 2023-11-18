64eefc6ba30adafc33835530f8a70a4f18ee3299
Col-E <mcoley2@gmu.edu>
2023-11-18 13:55:18 -0500

    Rewrite analysis simulation control flow

--- jasm-composition-jvm/src/main/java/me/darknet/assembler/compile/analysis/FrameState.java
+++ jasm-composition-jvm/src/main/java/me/darknet/assembler/compile/analysis/FrameState.java
@@ -1,5 +1,6 @@
 package me.darknet.assembler.compile.analysis;
 
+import me.darknet.assembler.compiler.InheritanceChecker;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Supplier;
@@ -14,28 +15,46 @@ public interface FrameState {
 	Frame getLastFrame();
 
 	/**
-	 * @param index Key.
+	 * @param index
+	 * 		Key.
+	 *
 	 * @return Frame at index.
 	 */
 	Frame getFrame(int index);
 
 	/**
-	 * @param index Key.
-	 * @param frameSupplier Lookup for frame to put, if index has no associated frame.
-	 * @return Frame at index.
+	 * Put and merge a given frame into the given index.
+	 *
+	 * @param index
+	 * 		Key.
+	 * @param frame
+	 * 		Frame to put at the index.
+	 * 		Will be merged into the existing frame at the index if a frame exists at the index.
+	 * @param checker
+	 * 		Inheritance checker for frame merging.
+	 *
+	 * @return The frame at the index. Will be {@code frame} if no prior frame existed at the index.
+	 * Will be the existing frame, after merging with {@code frame} if it existed previously at the index.
 	 */
-	default Frame putFrameIfAbsent(int index, @NotNull Supplier<Frame> frameSupplier) {
-		Frame frame = getFrame(index);
-		if (frame == null) {
-			frame = frameSupplier.get();
+	default boolean mergeInto(int index, @NotNull Frame frame, @NotNull InheritanceChecker checker) {
+		// If there is no existing frame, there is no merge to be done.
+		Frame existing = getFrame(index);
+		if (existing == null) {
 			putFrame(index, frame);
+			return false;
 		}
-		return frame;
+
+		Frame mergeTarget = existing.copy();
+		boolean changed = mergeTarget.merge(checker, frame);
+		if (changed) putFrame(index, mergeTarget);
+		return changed;
 	}
 
 	/**
-	 * @param index Key.
-	 * @param frame Frame to put.
+	 * @param index
+	 * 		Key.
+	 * @param frame
+	 * 		Frame to put.
 	 */
 	void putFrame(int index, Frame frame);
 }
--- jasm-composition-jvm/src/main/java/me/darknet/assembler/compile/analysis/jvm/AnalysisSimulation.java
+++ jasm-composition-jvm/src/main/java/me/darknet/assembler/compile/analysis/jvm/AnalysisSimulation.java
@@ -2,7 +2,7 @@ package me.darknet.assembler.compile.analysis.jvm;
 
 import dev.xdark.blw.code.*;
 import dev.xdark.blw.code.instruction.BranchInstruction;
-import dev.xdark.blw.code.instruction.ImmediateJumpInstruction;
+import dev.xdark.blw.code.instruction.ConditionalJumpInstruction;
 import dev.xdark.blw.simulation.ExecutionEngines;
 import dev.xdark.blw.simulation.Simulation;
 import dev.xdark.blw.type.InstanceType;
@@ -10,117 +10,170 @@ import dev.xdark.blw.type.Types;
 import me.darknet.assembler.compile.analysis.Frame;
 import me.darknet.assembler.compile.analysis.LocalInfo;
 import me.darknet.assembler.compiler.InheritanceChecker;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayDeque;
-import java.util.BitSet;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
-public class AnalysisSimulation implements Simulation<JvmAnalysisEngine, AnalysisSimulation.Info> {
-    @Override
-    public void execute(JvmAnalysisEngine engine, Info method) {
-        Deque<ForkKey> forkQueue = new ArrayDeque<>();
+public class AnalysisSimulation implements Simulation<JvmAnalysisEngine, AnalysisSimulation.Info>, JavaOpcodes {
+	private static final int MAX_QUEUE = 2048;
 
-        // Initial frame state holds local variables from parameters.
-        // We'll queue up the first instruction as a fork-point.
-        Frame initialFrame;
-        {
-            Frame frame = new Frame();
-            int index = 0;
-            for (LocalInfo param : method.params) {
-                int idx = index++;
-                if (param == null) // top
-                    continue;
-                frame.setLocal(idx, param);
-            }
-            forkQueue.push(new ForkKey(0, frame));
-            initialFrame = frame;
-            engine.putFrame(0, frame);
-        }
+	@Override
+	public void execute(JvmAnalysisEngine engine, AnalysisSimulation.Info method) {
+		final InheritanceChecker checker = method.checker();
+		final ForkQueue forkQueue = new ForkQueue(checker);
 
-        // Next we'll queue the catch blocks as fork-points.
-        // We know the stack will only contain a throwable type.
-        List<CodeElement> elements = method.method;
-        for (TryCatchBlock handler : method.exceptionHandlers) {
-            Label h = handler.handler();
-            int idx = elements.indexOf(h);
-            Frame copy = initialFrame.copy();
-            InstanceType type = handler.type();
-            if (type == null)
-                type = Types.instanceType(Throwable.class);
-            copy.push(type);
-            forkQueue.push(new ForkKey(idx, copy));
-        }
+		// Initial frame state holds local variables from parameters.
+		// We'll queue up the first instruction as a fork-point.
+		final Frame initialFrame;
+		{
+			initialFrame = new Frame();
+			int index = 0;
+			for (LocalInfo param : method.params()) {
+				int idx = index++;
+				if (param == null) continue; // top
+				initialFrame.setLocal(idx, param);
+			}
+			forkQueue.add(new ForkKey(0, initialFrame));
+		}
 
-        // Now we'll take each item out of the queue and continue until we hit
-        // some code that should create another fork-point (like control flow).
-        //
-        // Because control flow will allow us to revisit things we already analyzed
-        // with a potentially new state, we'll only revisit if the state has changed.
-        int numElements = elements.size();
-        BitSet visited = new BitSet(numElements);
-        ForkKey key;
-        loop:
-        while ((key = forkQueue.poll()) != null) {
-            int index = key.index;
-            Frame frame = key.frame.copy();
-            Frame oldFrame = engine.putFrameIfAbsent(index, frame::copy);
+		// Next we'll queue the catch blocks as fork-points.
+		// We know the stack will only contain a throwable type.
+		final List<CodeElement> elements = method.method();
+		final int elementCount = elements.size();
+		for (TryCatchBlock handler : method.exceptionHandlers()) {
+			Label handlerLabel = handler.handler();
+			int handlerIndex = elements.indexOf(handlerLabel);
+			InstanceType type = handler.type();
+			if (type == null)
+				type = Types.instanceType(Throwable.class);
 
-            // Only re-check code if the frame state has changed.
-            boolean skipVisited;
-            if (!oldFrame.equals(frame)) {
-                boolean changed = frame.merge(method.checker, oldFrame);
-                if (changed)
-                    engine.putFrame(index, frame.copy());
-                skipVisited = !changed;
-            } else {
-                skipVisited = true;
-            }
+			Frame frame = initialFrame.copy();
+			frame.push(type);
+			forkQueue.add(new ForkKey(handlerIndex, frame));
+		}
 
-            // Move forward in the simulation.
-            while (true) {
-                if (skipVisited && visited.get(index)) continue loop;
+		// Populate initial frame states for existing fork-keys.
+		for (ForkKey fork : forkQueue) {
+			engine.putFrame(fork.index(), fork.frame());
+		}
 
-                // Labels are fork-points, queue it and continue to the next point.
-                CodeElement element = elements.get(index);
-                if (element instanceof Label lbl) {
-                    // Do not queue if it is the last element.
-                    if (index <= numElements - 1)
-                        forkQueue.push(new ForkKey(lbl.index() + 1, frame));
-                    continue loop;
-                }
+		// Visit all queued fork-points. As we execute the code we may discover new points which to visit.
+		// We will continue to poll the next item and execute sequentially until we see the frame merging
+		// at fork-points results in no-changes.
+		ForkKey fork;
+		final BitSet visited = new BitSet(elementCount);
+		while ((fork = forkQueue.next()) != null) {
+			// Exit if we're getting out of control.
+			if (forkQueue.size() > MAX_QUEUE)
+				throw new IllegalStateException("Exceeded max queue size in stack simulation: " + MAX_QUEUE);
 
-                // Update the frame in the analyis engine, mark it as visited.
-                engine.putFrame(index, frame);
-                visited.set(index++);
+			// Get the initial state at this fork-point.
+			int index = fork.index;
+			Frame frame = engine.getFrame(index);
 
-                // Standard instructions update the frame state
-                boolean exit = false;
-                if (element instanceof Instruction insn) {
-                    ExecutionEngines.execute(engine, insn);
-                    int opcode = insn.opcode();
-                    exit = (opcode >= JavaOpcodes.IRETURN && opcode <= JavaOpcodes.RETURN)
-                            || opcode == JavaOpcodes.ATHROW
-                            || insn instanceof ImmediateJumpInstruction;
-                }
+			// Execute sequentially until hitting a fork-point with forcefully directed (or terminating) flow.
+			while (index < elementCount) {
+				frame = frame.copy();
+				CodeElement element = elements.get(index);
 
-                // Control flow instructions add new fork-points
-                if (element instanceof BranchInstruction bi) {
-                    bi.allTargets().forEach(target -> forkQueue.push(new ForkKey(elements.indexOf(target), frame.copy())));
-                }
+				Frame existingFrame = engine.getFrame(index);
+				if (existingFrame != null) {
+					// We need to merge our frame with the existing one to ensure types in the
+					// local variable table and stack are common to all execution paths.
+					//
+					// We also check for the existing frame merging with ours to see if we need
+					// to overwrite the existing frame with a more up-to-date state.
+					try {
+						boolean changed = frame.merge(checker, existingFrame)
+								|| existingFrame.copy().merge(checker, frame);
 
-                // If we hit a return instruction, we goto the next fork-point.
-                if (exit) {
-                    continue loop;
-                }
-            }
-        }
-    }
+						// We can continue the sequential execution if the code has already been visited
+						// and there were no changes in the merge process.
+						if (!changed && visited.get(index))
+							break;
+					} catch (Throwable t) {
+						throw new RuntimeException(t);
+					}
+				}
 
-    public record ForkKey(int index, Frame frame) {
-    }
+				// Mark this index as visited, then increment the index.
+				// We will be putting the frame at the 'next' index since the frame after is what sees
+				// the results of the prior instruction's execution. The 'frame' instance is shared below
+				// in the instruction execution logic.
+				visited.set(index++);
+				engine.putFrame(index, frame);
 
-    public record Info(InheritanceChecker checker, List<LocalInfo> params, List<CodeElement> method,
-                       List<TryCatchBlock> exceptionHandlers) {
-    }
+				if (element instanceof BranchInstruction bi) {
+					ExecutionEngines.execute(engine, bi);
+
+					// Create fork-points for all target labels.
+					Frame postExecutedFrame = frame;
+					bi.allTargets().forEach(target -> {
+						int targetIndex = elements.indexOf(target);
+						forkQueue.add(new ForkKey(targetIndex, postExecutedFrame));
+						boolean changed = engine.mergeInto(targetIndex, postExecutedFrame, checker);
+						if (changed) visited.clear(targetIndex);
+					});
+
+					// Break if control flow does not have fall-through case.
+					if (!(bi instanceof ConditionalJumpInstruction))
+						break;
+				} else if (element instanceof Instruction insn) {
+					ExecutionEngines.execute(engine, insn);
+
+					// Abort if control flow is terminal.
+					int opcode = insn.opcode();
+					if (opcode == ATHROW || (opcode >= IRETURN && opcode <= RETURN))
+						break;
+				}
+			}
+		}
+	}
+
+	private static class ForkQueue implements Iterable<ForkKey> {
+		private final NavigableMap<Integer, ForkKey> forkQueue = new TreeMap<>();
+		private final InheritanceChecker checker;
+
+		public ForkQueue(@NotNull InheritanceChecker checker) {
+			this.checker = checker;
+		}
+
+		public void add(@NotNull ForkKey key) {
+			forkQueue.merge(key.index, key, (a, b) -> {
+				Frame frameA = a.frame().copy();
+				Frame frameB = b.frame();
+				frameA.merge(checker, frameB);
+				return new ForkKey(a.index, frameA);
+			});
+		}
+
+		public int size() {
+			return forkQueue.size();
+		}
+
+		@Nullable
+		public ForkKey next() {
+			var entry = forkQueue.pollLastEntry();
+			if (entry == null) return null;
+			return entry.getValue();
+		}
+
+		@NotNull
+		@Override
+		public Iterator<ForkKey> iterator() {
+			return forkQueue.values().iterator();
+		}
+	}
+
+	private record ForkKey(int index, @NotNull Frame frame) implements Comparable<ForkKey> {
+		@Override
+		public int compareTo(@NotNull AnalysisSimulation.ForkKey other) {
+			return Integer.compare(index, other.index);
+		}
+	}
+
+	public record Info(InheritanceChecker checker, List<LocalInfo> params, List<CodeElement> method,
+					   List<TryCatchBlock> exceptionHandlers) {
+	}
 }
