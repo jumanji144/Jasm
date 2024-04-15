61f5f580c1652fae66de0c6b9c869c70ffb4165a
Justus Garbe <gihihoh@gmail.com>
2024-04-15 19:01:41 +0200

    fix: exit with code 1 when exception occurs

--- jasm-cli/src/main/java/me/darknet/assembler/cli/JasmCli.java
+++ jasm-cli/src/main/java/me/darknet/assembler/cli/JasmCli.java
@@ -9,7 +9,11 @@ public class JasmCli {
     public static void main(String[] args) {
         CommandLine commandLine = new CommandLine(new MainCommand());
         commandLine.setCaseInsensitiveEnumValuesAllowed(true);
-        commandLine.execute(args);
+        commandLine.setExecutionExceptionHandler((e, cmd, parseResult) -> {
+            e.printStackTrace();
+            return 1;
+        });
+        System.exit(commandLine.execute(args));
     }
 
 }
