1aa7cd2ce09c91cd8da52c412152cf8b7eadd67a
Matt <mcoley2@gmu.edu>
2024-10-26 02:37:24 -0400

    fix: Prevent duplicate escape handling on variable names

--- jasm-composition-jvm/src/main/java/me/darknet/assembler/printer/InstructionPrinter.java
+++ jasm-composition-jvm/src/main/java/me/darknet/assembler/printer/InstructionPrinter.java
@@ -131,7 +131,17 @@ public class InstructionPrinter implements IndexedExecutionEngine {
     public void execute(VarInstruction instruction) {
         int opcode = instruction.opcode();
         String varName = computeName(opcode, instruction.variableIndex(), currentIndex + 1);
-        ctx.instruction(OPCODES[opcode]).literal(varName).next();
+
+        ctx.instruction(OPCODES[opcode]);
+
+        // If it has already been escaped (\\uXXXX), print the escape as-is.
+        // We do not need to escape the variable name twice.
+        if (varName.matches("\\\\u[0-9a-fA-F]{4}"))
+            ctx.print(varName);
+        else
+            ctx.literal(varName);
+
+        ctx.next();
     }
 
     @Override
