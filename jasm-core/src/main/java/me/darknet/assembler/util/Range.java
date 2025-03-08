3d525ec448297940bbab84aad2f6b3588a41b0de
Matt <mcoley2@gmu.edu>
2025-03-07 20:31:52 -0500

    impr: Add overlap method to range

--- jasm-core/src/main/java/me/darknet/assembler/util/Range.java
+++ jasm-core/src/main/java/me/darknet/assembler/util/Range.java
@@ -1,7 +1,8 @@
 package me.darknet.assembler.util;
 
-public record Range(int start, int end) {
+import org.jetbrains.annotations.NotNull;
 
+public record Range(int start, int end) {
     public static final Range EMPTY = new Range(-1, -1);
 
     public boolean within(int pos) {
@@ -10,5 +11,13 @@ public record Range(int start, int end) {
 
     public boolean withinExclusive(int pos) {
         return pos > start && pos < end;
+    }
+
+    public boolean overlap(@NotNull Range other) {
+        return Math.max(start, other.start) <= Math.max(end, other.end);
+    }
+
+    public static boolean overlap(int start, int end, int otherStart, int otherEnd) {
+        return Math.max(start, otherStart) <= Math.max(end, otherEnd);
     }
 }
